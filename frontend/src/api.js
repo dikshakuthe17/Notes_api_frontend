cf8e219ad4ef50582872e3b8453df6435061987c
Diksha Shamrao Kuthe <kuthediksha123@gmail.com>
2025-08-11 06:17:02 +0530

    update all the changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api", // backend ka URL
+    baseURL: "https://notes-api-backend-roan.vercel.app/api", // backend ka URL
 });
